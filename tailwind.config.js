14fd7db1592816f3401b7b8983b83bbc1043df87
Carlos Baso <110511071+Basso-23@users.noreply.github.com>
2024-05-06 09:56:49 -0500

    chnages

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,8 +13,24 @@ module.exports = {
       center: true,
       padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        "3xl": "1500px",
       },
+    },
+    screens: {
+      sm: "900px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "1000px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1500px",
+      // => @media (min-width: 1536px) { ... }
     },
     extend: {
       colors: {
